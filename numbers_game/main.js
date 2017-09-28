5f0c0cf1d1f3a4d7d2f1a6a3c7a58997fd572145
Charles Julian <cjulian@bermangrp.com>
2017-09-27 22:17:05 -0400

    styles added tweaked resetgame variables

--- numbers_game/main.js
+++ numbers_game/main.js
@@ -13,14 +13,21 @@
 		var randomNumber = Math.floor(Math.random() * 100) +1;
 		// resuts DOM
 		var guesses = document.querySelector('.guesses');
+		var guessNumber = document.querySelector('.guessNumber')
+		var resultParams = document.querySelector('.resultParams');
+		var lastResultContainer = document.querySelector('.lastResultContainer');
 		var lastResult = document.querySelector('.lastResult');
+		var incorrect = document.querySelector('.incorrect');
 		var lowOrHi = document.querySelector('.lowOrHi');
+		var lowOrHiContainer = document.querySelector('.lowOrHiContainer');
+		var lowGuessP = document.querySelector('.lowGuessP');
 		var lowGuess = document.querySelector('.lowGuess');
 		var binary_search = $('<a/>');
 		binary_search.text('Binary Search');
 		var binary_search_url = 'https://www.wikiwand.com/en/Binary_search_algorithm#/History';
 		binary_search.attr('target', "blank");
 		binary_search.attr('href', binary_search_url);
+		var new_game_button = document.querySelector('.newGame');
 
 		// guess DOM
 		var guessSubmit = document.querySelector('.guessSubmit');
@@ -47,7 +54,7 @@
 		    if (guessCount === 1) {
 		        guesses.textContent = 'Previous guesses: ';
 		    }
-		    guesses.textContent += userGuess + ' ';
+		    guessNumber.textContent += userGuess + ' ';
 
 		    if (userGuess === randomNumber) {
 		        lastResult.textContent = `Congratulations! You got it right in ${guessCount} tries! The Number is ${randomNumber}`;
@@ -60,20 +67,44 @@
 		        document.querySelector('.lowGuess').classList.add('displayNone');
 		        $('.binary_help').append(binary_search);
 		        $('.binary_block').removeClass('displayNone')
+		        $('.lastResultContainer').removeClass('fadeOut');
 
 		        setGameOver();
-		    } else if(guessCount >=7 && guessCount > 0){
-		    	console.log(guessCount);
-		    	var guessesLeft = 10 - guessCount
-		    	lowGuess.textContent = `You have ${guessesLeft} guesses left`;
-		    	document.querySelector('.lowGuess').classList.remove('displayNone');
+		    
 		    } else {
+		        resultParams.classList.remove('displayNone');
 		        lastResult.textContent = 'Wrong!';
-		        lastResult.style.backgroundColor = 'red';
 		        if(userGuess < randomNumber) {
+		        	lastResultContainer.classList.remove('fadeOut');
+		        	incorrect.classList.remove('fadeOut');
+		        	lowOrHiContainer.classList.remove('lowAnswer');
+		        	lowOrHiContainer.classList.remove('highAnswer');
+		        	lowOrHiContainer.classList.add('lowAnswer');
 		            lowOrHi.textContent = 'Last guess was too low!';
+
+		            setTimeout(function(){ 
+		            	$('.lastResultContainer').toggleClass('fadeOut');
+		            	$('.incorrect').toggleClass('fadeOut');
+		            	console.log(guessCount)
+
+		            }, 1000);
+		            
 		        } else if(userGuess > randomNumber) {
+		        	console.log(lastResultContainer)
+		        	console.log("lastResultContainer")
+		        	lastResultContainer.classList.remove('fadeOut');
+		        	incorrect.classList.remove('fadeOut');
+		            lowOrHiContainer.classList.remove('lowAnswer');
+		        	lowOrHiContainer.classList.remove('highAnswer');
+		            lowOrHiContainer.classList.add('highAnswer');
 		            lowOrHi.textContent = 'Last guess was too high!';
+		            setTimeout(function(){ 
+		            	$('.lastResultContainer').toggleClass('fadeOut');
+		            	$('.incorrect').toggleClass('fadeOut');
+
+
+		            }, 1000);
+		            
 		        }
 		    }
 
@@ -86,10 +117,13 @@
 		function setGameOver() {
 		    guessField.disabled = true;
 		    guessSubmit.disabled = true;
-		    resetButton = document.createElement('button');
-		    resetButton.textContent = 'Start new game';
-		    document.querySelector('.container').appendChild(resetButton);
-		    resetButton.addEventListener('click', resetGame);
+		    // resetButton = document.createElement('button');
+		    // resetButton.textContent = 'Start new game';
+		    // document.querySelector('.container').appendChild(resetButton);
+		    // resetButton.addEventListener('click', resetGame);
+		    console.log(new_game_button)
+		    new_game_button.classList.remove('displayNone');
+		    new_game_button.addEventListener('click', resetGame);
 		}
 
 		function resetGame() {
@@ -100,16 +134,18 @@
 		        resetParams[i].textContent = '';
 		    }
 
-		    resetButton.parentNode.removeChild(resetButton);
-
 		    guessField.disabled = false;
 		    guessSubmit.disabled = false;
 		    guessField.value = '';
 		    guessField.focus();
 
-		    lastResult.style.backgroundColor = 'white';
-
 		    randomNumber = Math.floor(Math.random() * 100) + 1;
+
+		    document.querySelector('.resultParams').classList.add('displayNone')
+		    document.querySelector('.binary_block').classList.add('displayNone')
+		    document.querySelector('.guessNumber').textContent = "";
+		    
+		    new_game_button.classList.add('displayNone');
 		}
 
 		var init = function(){
